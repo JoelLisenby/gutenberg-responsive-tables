cef9b562ec0cd5d64d2259be4f374114abee7205
Joel Lisenby <joel.lisenby@gmail.com>
2026-05-09 11:05:12 -0700

    Fix JSX error

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -2,41 +2,44 @@ const { addFilter } = wp.hooks;
 const { createHigherOrderComponent } = wp.compose;
 const { InspectorControls } = wp.blockEditor;
 const { PanelBody, TextControl } = wp.components;
-const { Fragment } = wp.element;
+const { createElement, Fragment } = wp.element;
 
-const withTableResponsiveControls = createHigherOrderComponent( ( BlockEdit ) => {
-    return ( props ) => {
-        if ( props.name !== 'core/table' ) {
-            return <BlockEdit {...props} />;
+const withTableResponsiveControls = createHigherOrderComponent((BlockEdit) => {
+    return (props) => {
+        if (props.name !== 'core/table') {
+            return createElement(BlockEdit, props);
         }
 
         const { attributes, setAttributes } = props;
         const { responsiveBreakpoint } = attributes;
 
-        return (
-            <Fragment>
-                <BlockEdit {...props} />
-
-                <InspectorControls>
-                    <PanelBody title="Responsive Cards" initialOpen={ false }>
-                        <TextControl
-                            label="Mobile Breakpoint (px)"
-                            help="Leave empty to use automatic theme detection. Example: 768"
-                            value={ responsiveBreakpoint || '' }
-                            onChange={ ( value ) => {
-                                const num = parseInt( value, 10 );
-                                setAttributes( {
-                                    responsiveBreakpoint: isNaN( num ) ? null : num,
-                                } );
-                            } }
-                            type="number"
-                        />
-                    </PanelBody>
-                </InspectorControls>
-            </Fragment>
+        return createElement(
+            Fragment,
+            null,
+            createElement(BlockEdit, props),
+            createElement(
+                InspectorControls,
+                null,
+                createElement(
+                    PanelBody,
+                    { title: 'Responsive Cards', initialOpen: false },
+                    createElement(TextControl, {
+                        label: 'Mobile Breakpoint (px)',
+                        help: 'Leave empty to use automatic theme detection. Example: 768',
+                        value: responsiveBreakpoint || '',
+                        type: 'number',
+                        onChange: (value) => {
+                            const num = parseInt(value, 10);
+                            setAttributes({
+                                responsiveBreakpoint: isNaN(num) ? null : num,
+                            });
+                        },
+                    })
+                )
+            )
         );
     };
-}, 'withTableResponsiveControls' );
+}, 'withTableResponsiveControls');
 
 addFilter(
     'editor.BlockEdit',
